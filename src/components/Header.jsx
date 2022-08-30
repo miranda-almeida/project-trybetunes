850f5040b5e76ea4501c44a72aad1b89a8e177ad
miranda <magui@duck.com>
2022-08-30 16:58:18 -0300

    requisito 4: links de navegação no header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -35,6 +36,15 @@ export default class Header extends Component {
               {user.name}
             </h2>
           )}
+        <Link data-testid="link-to-search" to="/search">
+          Search
+        </Link>
+        <Link data-testid="link-to-favorites" to="/favorites">
+          Favorites
+        </Link>
+        <Link data-testid="link-to-profile" to="/profile">
+          Profile
+        </Link>
       </div>
     );
   }
