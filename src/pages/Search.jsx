4fbec01e8a05d25eaddad74152cdd6007d0ba7eb
miranda <magui@duck.com>
2022-08-30 17:10:14 -0300

    requisito 5: adiciona input para pesquisar artista no Search component

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,33 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 export default class Search extends Component {
+  // seta estado inicial da pesquisa inserida (input vazio)
+  state = {
+    searchInput: '',
+  };
+
   render() {
+    // define constante para evitar magic number
+    const MIN_LENGTH = 2;
+    // desestrutura chave do estado inicial
+    const { searchInput } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
         <h1>Search</h1>
+        <input
+          data-testid="search-artist-input"
+          // indica e desestrutura o valor inserido como valor da chave para a lógica de habilitação do botão
+          onChange={ ({ target: { value } }) => this.setState({ searchInput: value }) }
+        />
+        <button
+          type="button"
+          data-testid="search-artist-button"
+          // seta a condição mínima de dois caracteres para habilitar o botão
+          disabled={ searchInput.length < MIN_LENGTH }
+        >
+          Pesquisar
+        </button>
       </div>
     );
   }
